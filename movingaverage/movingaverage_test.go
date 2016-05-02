3960dbca6fa0ab14905e56c24d6e0baca3bcb6a9
Tiffany Jernigan <tiffany.jernigan@intel.com>
2016-05-01 20:57:07 -0700

    New metric schema

--- movingaverage/movingaverage_test.go
+++ movingaverage/movingaverage_test.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/intelsdi-x/snap/control/plugin"
 	"github.com/intelsdi-x/snap/control/plugin/cpolicy"
+	"github.com/intelsdi-x/snap/core"
 	"github.com/intelsdi-x/snap/core/ctypes"
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -81,41 +82,41 @@ func TestMovingAverageProcessor(t *testing.T) {
 
 func TestMovingAverageProcessorMetrics(t *testing.T) {
 	Convey("Moving Average Processor tests", t, func() {
-		metrics := make([]plugin.PluginMetricType, 10)
+		metrics := make([]plugin.MetricType, 10)
 		config := make(map[string]ctypes.ConfigValue)
 
 		config["MovingAvgBufLength"] = ctypes.ConfigValueInt{Value: -1}
 
 		Convey("Moving average for int data", func() {
-			for i, _ := range metrics {
+			for i := range metrics {
 				time.Sleep(3)
 				rand.Seed(time.Now().UTC().UnixNano())
 				data := randInt(65, 90)
-				metrics[i] = *plugin.NewPluginMetricType([]string{"foo", "bar"}, time.Now(), "", nil, nil, data)
+				metrics[i] = *plugin.NewMetricType(core.NewNamespace("foo", "bar"), time.Now(), nil, "some unit", data)
 			}
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
 			enc.Encode(metrics)
 			movingAverageObj := NewMovingaverageProcessor()
 
-			_, received_data, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), config)
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), config)
 
-			var metrics_new []plugin.PluginMetricType
+			var metricsNew []plugin.MetricType
 
-			//Decodes the content into pluginMetricType
-			dec := gob.NewDecoder(bytes.NewBuffer(received_data))
-			dec.Decode(&metrics_new)
-			So(metrics, ShouldNotResemble, metrics_new)
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
 
 		})
 
 		Convey("Moving average for float32 data", func() {
 			config["MovingAvgBufLength"] = ctypes.ConfigValueInt{Value: 40}
-			for i, _ := range metrics {
+			for i := range metrics {
 				time.Sleep(3)
 				rand.Seed(time.Now().UTC().UnixNano())
 				data := randInt(65, 90)
-				metrics[i] = *plugin.NewPluginMetricType([]string{"foo", "bar"}, time.Now(), "", nil, nil, float32(data))
+				metrics[i] = *plugin.NewMetricType(core.NewNamespace("foo", "bar"), time.Now(), nil, "some unit", float32(data))
 			}
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
@@ -123,22 +124,22 @@ func TestMovingAverageProcessorMetrics(t *testing.T) {
 
 			movingAverageObj := NewMovingaverageProcessor()
 
-			_, received_data, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), config)
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), config)
 
-			var metrics_new []plugin.PluginMetricType
+			var metricsNew []plugin.MetricType
 
-			//Decodes the content into pluginMetricType
-			dec := gob.NewDecoder(bytes.NewBuffer(received_data))
-			dec.Decode(&metrics_new)
-			So(metrics, ShouldNotResemble, metrics_new)
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
 
 		})
 		Convey("Moving average for float64 data", func() {
-			for i, _ := range metrics {
+			for i := range metrics {
 				time.Sleep(3)
 				rand.Seed(time.Now().UTC().UnixNano())
 				data := randInt(65, 90)
-				metrics[i] = *plugin.NewPluginMetricType([]string{"foo", "bar"}, time.Now(), "", nil, nil, float64(data))
+				metrics[i] = *plugin.NewMetricType(core.NewNamespace("foo", "bar"), time.Now(), nil, "some unit", float64(data))
 			}
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
@@ -146,68 +147,68 @@ func TestMovingAverageProcessorMetrics(t *testing.T) {
 
 			movingAverageObj := NewMovingaverageProcessor()
 
-			_, received_data, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
 
-			var metrics_new []plugin.PluginMetricType
+			var metricsNew []plugin.MetricType
 
-			//Decodes the content into pluginMetricType
-			dec := gob.NewDecoder(bytes.NewBuffer(received_data))
-			dec.Decode(&metrics_new)
-			So(metrics, ShouldNotResemble, metrics_new)
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
 
 		})
 
 		Convey("Moving average for uint32 data", func() {
-			for i, _ := range metrics {
+			for i := range metrics {
 				time.Sleep(3)
 				rand.Seed(time.Now().UTC().UnixNano())
 				data := randInt(65, 90)
-				metrics[i] = *plugin.NewPluginMetricType([]string{"foo", "bar"}, time.Now(), "", nil, nil, uint32(data))
+				metrics[i] = *plugin.NewMetricType(core.NewNamespace("foo", "bar"), time.Now(), nil, "some unit", uint32(data))
 			}
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
 			enc.Encode(metrics)
 			movingAverageObj := NewMovingaverageProcessor()
 
-			_, received_data, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
 
-			var metrics_new []plugin.PluginMetricType
+			var metricsNew []plugin.MetricType
 
-			//Decodes the content into pluginMetricType
-			dec := gob.NewDecoder(bytes.NewBuffer(received_data))
-			dec.Decode(&metrics_new)
-			So(metrics, ShouldNotResemble, metrics_new)
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
 
 		})
 
 		Convey("Moving average for uint64 data", func() {
-			for i, _ := range metrics {
+			for i := range metrics {
 				time.Sleep(3)
 				rand.Seed(time.Now().UTC().UnixNano())
 				data := randInt(65, 90)
-				metrics[i] = *plugin.NewPluginMetricType([]string{"foo", "bar"}, time.Now(), "", nil, nil, uint64(data))
+				metrics[i] = *plugin.NewMetricType(core.NewNamespace("foo", "bar"), time.Now(), nil, "some unit", uint64(data))
 			}
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
 			enc.Encode(metrics)
 			movingAverageObj := NewMovingaverageProcessor()
 
-			_, received_data, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
 
-			var metrics_new []plugin.PluginMetricType
+			var metricsNew []plugin.MetricType
 
-			//Decodes the content into pluginMetricType
-			dec := gob.NewDecoder(bytes.NewBuffer(received_data))
-			dec.Decode(&metrics_new)
-			So(metrics, ShouldNotResemble, metrics_new)
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
 
 		})
 
 		Convey("Moving average for unknown data type", func() {
-			for i, _ := range metrics {
+			for i := range metrics {
 
 				data := "I am an unknow data Type"
-				metrics[i] = *plugin.NewPluginMetricType([]string{"foo", "bar"}, time.Now(), "", nil, nil, data)
+				metrics[i] = *plugin.NewMetricType(core.NewNamespace("foo", "bar"), time.Now(), nil, "some unit", data)
 			}
 			var buf bytes.Buffer
 			enc := gob.NewEncoder(&buf)
@@ -215,14 +216,14 @@ func TestMovingAverageProcessorMetrics(t *testing.T) {
 
 			movingAverageObj := NewMovingaverageProcessor()
 
-			_, received_data, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
+			_, receivedData, _ := movingAverageObj.Process("snap.gob", buf.Bytes(), nil)
 
-			var metrics_new []plugin.PluginMetricType
+			var metricsNew []plugin.MetricType
 
-			//Decodes the content into pluginMetricType
-			dec := gob.NewDecoder(bytes.NewBuffer(received_data))
-			dec.Decode(&metrics_new)
-			So(metrics, ShouldNotResemble, metrics_new)
+			//Decodes the content into MetricType
+			dec := gob.NewDecoder(bytes.NewBuffer(receivedData))
+			dec.Decode(&metricsNew)
+			So(metrics, ShouldNotResemble, metricsNew)
 
 		})
 
